8d34c9412164831c728200ba87c9757f5350997b
Giorgino Malik <giorginomalik@gmail.com>
2025-06-24 09:37:46 +0000

    html

--- cek_tidak_followback.py
+++ cek_tidak_followback.py
@@ -1,20 +1,31 @@
 import re
 
+def extract_usernames_and_links(filename):
+    with open(filename, 'r', encoding='utf-8') as f:
+        html = f.read()
+    # Ambil username dan link lengkap
+    pattern = r'<a[^>]+href="(https://www.instagram.com/([\w\.]+))"[^>]*>'
+    return dict(re.findall(pattern, html))
+
 def extract_usernames(filename):
     with open(filename, 'r', encoding='utf-8') as f:
         html = f.read()
-    # Asumsi username ada dalam href ke instagram.com/username
     usernames = set(re.findall(r'instagram.com/([\w\.]+)', html))
     return usernames
 
 if __name__ == "__main__":
     followers = extract_usernames('followers_1.html')  # yang mengikuti Anda
-    following = extract_usernames('following.html')    # yang Anda ikuti
+    following_links = extract_usernames_and_links('following.html')  # yang Anda ikuti (username & link)
+    following = set(following_links.keys())
 
     not_following_back = following - followers
 
-    with open('tidak_followback.txt', 'w', encoding='utf-8') as out:
-        out.write("Akun yang Anda ikuti tetapi tidak mengikuti Anda kembali:\n")
-        for user in sorted(not_following_back):
-            out.write(user + '\n')
-    print("Daftar sudah disimpan di file 'tidak_followback.txt'")
+    with open('tidak_followback.html', 'w', encoding='utf-8') as out:
+        out.write('<html><head><title>Tidak Followback</title></head><body>')
+        out.write('<h2>Akun yang Anda ikuti tetapi tidak mengikuti Anda kembali:</h2>')
+        out.write('<ul>')
+        for link in sorted(not_following_back):
+            url = following_links[link]
+            out.write(f'<li><a href="{url}" target="_blank">{link}</a></li>')
+        out.write('</ul></body></html>')
+    print("Daftar sudah disimpan di file 'tidak_followback.html'")
